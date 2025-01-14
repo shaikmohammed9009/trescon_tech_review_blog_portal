273c825857e2b99d0cd60f426bc4e495bc60993f
Muzammil Ahmed <muzammilahmed.166@gmail.com>
2025-01-14 16:15:39 +0530

    Delete project_11

--- project_11/lib/hooks/use-isomorphic-layout-effect.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { useEffect, useLayoutEffect } from 'react';
-
-// Use useLayoutEffect in browser, fallback to useEffect in SSR
-export const useIsomorphicLayoutEffect = 
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
